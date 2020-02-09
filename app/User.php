49740edd9ec59e9e7afb9b456ad5ac00c82c9fe8
Mark Warren <markwarren.dev@gmail.com>
2020-02-09 14:35:36 +0800

    user-post relationship

--- app/User.php
+++ app/User.php
@@ -37,4 +37,10 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    // Relationships
+    public function posts()
+    {
+        return $this->hasMany('App\Post');
+    }
 }
